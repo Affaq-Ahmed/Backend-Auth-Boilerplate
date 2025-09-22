23a4e731f72bcd7e2d407e36d9118839262d22e5
Affaq-Ahmed <affaqahmed165@gmail.com>
2025-09-22 17:39:56 +0500

    feat: add authentication routes for user registration, login, and token refresh

---
+++ routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as AuthController from '../modules/users/auth/auth.controller.js';
+
+const router = express.Router();
+
+router.post('/register', AuthController.register);
+router.post('/login', AuthController.login);
+router.post('/refresh-token', AuthController.refreshToken);
+
+export default router;
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ import UserRoutes from './user.route.js';
 const router = express.Router();
 
 router.use('/users', UserRoutes);
+router.use('/auth', UserRoutes);
 
 export default router;
